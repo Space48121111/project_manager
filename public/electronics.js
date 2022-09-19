cbe3e31032f59eb931cd855246734633d6b9504d
Stella <stellavir11@gmail.com>
2022-09-19 18:20:35 +0300

    Add files via upload
    Connections

--- public/electronics.js
+++ public/electronics.js
@@ -25,7 +25,7 @@ function drawContacts() {
   {
     // background fill
     ctx.beginPath();
-    ctx.fillStyle = 'white';
+    ctx.fillStyle = hoveredContact == i ? 'green' : 'white';
     ctx.arc(contactPoints[i].x, contactPoints[i].y, contactRadius, 2*Math.PI, 0);
     ctx.fill();
 
@@ -42,24 +42,47 @@ function drawContacts() {
     ctx.textAlign = 'center';
     ctx.fillText(contactPoints[i].type, contactPoints[i].x, contactPoints[i].y+5);
 
+
+	/*
+	ctx.strokeStyle = 'black';
+	ctx.lineWidth = 2;
+	ctx.beginPath();
+		
+	ctx.moveTo(contactPoints[i].x, contactPoints[i].y);
+	
+	if (contactPoints[i].horizontal)
+	{
+		ctx.lineTo(contactPoints[i].x + contactPoints[i].direction, contactPoints[i].y);
+	}
+	else 
+	{
+		ctx.lineTo(contactPoints[i].x, contactPoints[i].y + contactPoints[i].direction);
+	}
+	*/
+	
+	ctx.stroke();
   }
 }
 
-function contact(x, y, type) {
+function contact(x, y, type, horizontal, direction) {
   var obj = {
     x: x,
     y: y,
     type: type,
+	horizontal: horizontal,
+	direction: direction,
   };
   contactPoints.push(obj);
 }
 
 // add contacts into the array
 function initializeContact() {
-  contact(boardX+50, boardY, "+");
-  contact(boardX+100, boardY, "-");
-  contact(batteryX-15, batteryY+batteryHeight/2, "+");
-  contact(batteryX+batteryWidth+10, batteryY+batteryHeight/2, "-");
+  contact(boardX+50, boardY, "+", false, -20);
+  contact(boardX+100, boardY, "-", false, -25);
+  contact(boardX+150, boardY, "-", false, -30);
+  
+  contact(batteryX-15, batteryY+batteryHeight/2, "+", true, -20);
+  contact(batteryX+batteryWidth+10, batteryY+batteryHeight/2, "-", true, 20);
 
 }
 
@@ -86,32 +109,206 @@ function battery() {
 
 }
 
+function drawMakeNewConnection() 
+{
+	if (startConnection != -1)
+	{
+		ctx.strokeStyle = 'green';
+		ctx.lineWidth = 2;
+        ctx.beginPath();
+        ctx.moveTo(contactPoints[startConnection].x, contactPoints[startConnection].y);
+        ctx.lineTo(mouse_x, mouse_y);
+        ctx.stroke();
+	}
+}
 
+function drawConnections() 
+{
+	for (let i=0; i<conns.length; i++)
+	{
+		var start = conns[i].start;
+		var end = conns[i].end;
+		
+		/*
+		ctx.strokeStyle = 'black';
+		ctx.lineWidth = 2;
+        ctx.beginPath();	
+        ctx.moveTo(contactPoints[start].x, contactPoints[start].y);
+        ctx.lineTo(contactPoints[end].x, contactPoints[end].y);
+        ctx.stroke();	
+		*/
+		
+		ctx.strokeStyle = 'black';
+		ctx.lineWidth = 2;
+		
+		var start_x = 0;
+		var start_y = 0;
+		if (contactPoints[start].horizontal)
+		{
+			start_x = contactPoints[start].x + contactPoints[start].direction;
+			start_y = contactPoints[start].y;
+		
+		}
+		else 
+		{
+			start_x = contactPoints[start].x;
+			start_y = contactPoints[start].y + contactPoints[start].direction;
+		}	
+		
+		var end_x = 0;
+		var end_y = 0;
+		if (contactPoints[end].horizontal)
+		{
+			end_x = contactPoints[end].x + contactPoints[end].direction;
+			end_y = contactPoints[end].y;
+		}
+		else 
+		{
+			end_x = contactPoints[end].x;
+			end_y = contactPoints[end].y + contactPoints[end].direction;
+		}	
+			
+		//Draw starting pin
+		ctx.beginPath();
+		ctx.moveTo(contactPoints[start].x, contactPoints[start].y);	
+		ctx.lineTo(start_x, start_y);
+		ctx.stroke();
+		
+		//Draw ending pin
+		ctx.beginPath();
+		ctx.moveTo(contactPoints[end].x, contactPoints[end].y);	
+		ctx.lineTo(end_x, end_y);
+		ctx.stroke();	
+		
+		//
+		if (contactPoints[start].horizontal)
+		{
+			ctx.beginPath();
+			ctx.moveTo(contactPoints[start].x + contactPoints[start].direction, contactPoints[start].y);
+			ctx.lineTo(contactPoints[start].x + contactPoints[start].direction, end_y);	
+			ctx.stroke();
+		}
+		else 
+		{
+			ctx.beginPath();
+			ctx.moveTo(contactPoints[start].x, contactPoints[start].y + contactPoints[start].direction);
+			ctx.lineTo(end_x, contactPoints[start].y + contactPoints[start].direction);	
+			ctx.stroke();		
+		}	
+		
+		//
+		if (contactPoints[end].horizontal)
+		{
+			ctx.beginPath();
+			ctx.moveTo(contactPoints[end].x + contactPoints[end].direction, contactPoints[end].y);
+			ctx.lineTo(contactPoints[end].x + contactPoints[end].direction, start_y);	
+			ctx.stroke();
+		}
+		else 
+		{
+			ctx.beginPath();
+			ctx.moveTo(contactPoints[end].x, contactPoints[end].y + contactPoints[end].direction);
+			ctx.lineTo(start_x, contactPoints[end].y + contactPoints[end].direction);	
+			ctx.stroke();		
+		}	
+	}
+}
 
-
-function draw() {
+function draw() 
+{
   ctx.fillStyle = 'silver';
   ctx.beginPath();
   ctx.rect(0, 0, canvas.width, canvas.height);
   ctx.fill();
   drawBoard();
   battery();
+  drawConnections();
+  drawMakeNewConnection();
   drawContacts();
 
 }
 
-
-function frame() {
-  // updateSchematics();
-  draw();
-
+function startConnect() 
+{
+	if (hoveredContact != -1)
+	{
+		startConnection = hoveredContact;
+		isConnecting = true;
+	}
 }
 
+function stopConnect() 
+{
+	//
+	if (startConnection != -1 && hoveredContact != -1 && startConnection != hoveredContact)
+	{
+		addConnection(startConnection, hoveredContact);
+	}
+	
+	startConnection = -1;
+	isConnecting = false;
+}
 
+function mouseMovement(e) 
+{
+	var rect = canvas.getBoundingClientRect();
+	mouse_x = e.clientX - rect.left;
+	mouse_y = e.clientY - rect.top;
+}
 
+function updateHoveredContact() 
+{
+	let newContact = -1;
+	
+	for (var i=0; i<contactPoints.length; i++)
+	{
+		if (mouse_x >= contactPoints[i].x - contactRadius/2 && mouse_x <= contactPoints[i].x + contactRadius/2 &&
+			mouse_y >= contactPoints[i].y - contactRadius/2 && mouse_y <= contactPoints[i].y + contactRadius/2)
+		{
+			newContact = i;
+			break;
+		}
+	}
+	
+	if (newContact != hoveredContact)
+	{
+		hoveredContact = newContact;
+		console.log(hoveredContact);
+	}
+}
 
+function removeConnection(contact) 
+{
+	for (var i=conns.length-1; i>=0; i--)
+	{
+		if (conns[i].start == contact || conns[i].end == contact)
+		{
+			conns.splice(i, 1);
+		}
+	}
+}
 
+function addConnection(start, end) 
+{
+	removeConnection(start);
+	removeConnection(end);
+	
+	var obj = {
+		start: start,
+		end: end,
+	};	
+	
+	conns.push(obj);
+}
 
+function frame() 
+{
+	updateHoveredContact();
+	
+	// updateSchematics();
+	draw();
+
+}
 
 setInterval(function(){
   frame()
--- public/var_elec.js
+++ public/var_elec.js
@@ -10,3 +10,11 @@ let batteryWidth = 75;
 let batteryHeight = 50;
 let batteryX = 250;
 let batteryY = 20;
+
+let isConnecting = false;
+let mouse_x = 0;
+let mouse_y = 0;
+let hoveredContact = -1;
+let startConnection = -1;
+
+let conns = [];
